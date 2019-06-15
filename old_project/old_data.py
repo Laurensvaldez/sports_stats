7b8c78482de04936478924bd587ae1268cfe4cb5
Laurens <llj.salcedovaldez@gmail.com>
2019-06-15 11:46:03 +0200

    Initial commit

---
+++ old_project/old_data.py
@@ -0,0 +1,50 @@
+from player import Player
+
+# add the data of all the players in this file
+# order of the list = first name, second name, team, position, games, at bats, runs, hits, doubles, triples, home runs,
+#                  rbi, bb, so, sb, cs
+all_players = [
+    Player("miquel", "andujar", "nyy", "3b", 149, 573, 83, 170, 47, 2, 27, 92, 25, 97, 2, 1),
+    Player("greg", "bird","nyy","1b",82 ,272 ,23 ,54 ,16 ,1 ,11 , 38, 30, 78, 0, 0),
+    Player("clint", "frazier","nyy", "lf", 15, 34, 9, 9, 3, 0, 0, 1, 5, 13, 0, 0),
+    Player("brett", "gardner", "nyy",	"lf", 140, 530, 95, 125, 20, 7, 12, 45, 65, 107, 16, 2),
+    Player("didi", "gregorius", "nyy", "ss", 134, 504, 89, 135, 23, 5, 27, 86, 48, 69, 10, 6),
+    Player("adeiny", "hechavarria", "nyy", "ss", 94, 296, 34, 73, 11, 0, 6, 31, 16, 58, 2, 0),
+    Player("aaron", "hicks", "nyy", "cf", 137, 480, 90, 119, 18, 3, 27, 79, 90, 111, 11, 2),
+    Player("kyle", "higashioka", "nyy", "c",29, 72, 6, 12, 2, 0, 3, 6, 6, 16, 0, 0),
+    Player("aaron", "judge", "nyy", "rf",112, 413, 77, 115, 22, 0, 27, 67, 76, 152, 6, 3),
+    Player("andrew", "mccutchen", "nyy", "rf", 155, 569, 83, 145, 30, 3, 20, 65, 95, 145, 14, 9),
+    Player("shane", "robinson", "nyy", "lf", 25, 49, 8, 7, 1, 0, 1, 2, 4, 6, 1, 0),
+    Player("austin", "romine", "nyy", "c", 77, 242, 30, 59, 12, 0, 10, 42, 17, 67, 1, 0),
+    Player("gary", "sanchez", "nyy", "c", 89, 323, 51, 60, 17, 0, 18, 53, 46, 94, 1, 0),
+    Player("giancarlo", "stanton", "nyy", "dh", 158, 617, 102, 164, 34, 1, 38, 100, 70, 211, 5, 0),
+    Player("gleyber", "torres", "nyy", "2b", 123, 431, 54, 117, 16, 1, 24, 77, 42, 122, 6, 2),
+    Player("ronald", "torreyes", "nyy", "2b", 41, 100, 9, 28, 7, 1, 0, 7, 2, 16, 0, 0),
+    Player("giovanny", "urshela", "nyy", "3b", 19, 43, 7, 10, 1, 0, 1, 3, 2, 10, 0, 0),
+    Player("luke", "voit", "nyy", "1b", 47, 143, 30, 46, 5, 0, 15, 36, 17, 43, 0, 0),
+    Player("tyler", "wade", "nyy", "ss", 36, 66, 8, 11, 4, 0, 1, 5, 4, 23, 1, 0),
+    Player("neil", "walker", "nyy", "1b", 113, 347, 48, 76, 12, 1, 11, 46, 42, 87, 0, 0),
+]
+
+players_list = [
+("miquel", "andujar", "nyy", "3b", 149, 573, 83, 170, 47, 2, 27, 92, 25, 97, 2, 1),
+    ("greg", "bird","nyy","1b",82 ,272 ,23 ,54 ,16 ,1 ,11 , 38, 30, 78, 0, 0),
+    ("clint", "frazier","nyy", "lf", 15, 34, 9, 9, 3, 0, 0, 1, 5, 13, 0, 0),
+    ("brett", "gardner", "nyy",	"lf", 140, 530, 95, 125, 20, 7, 12, 45, 65, 107, 16, 2),
+    ("didi", "gregorius", "nyy", "ss", 134, 504, 89, 135, 23, 5, 27, 86, 48, 69, 10, 6),
+    ("adeiny", "hechavarria", "nyy", "ss", 94, 296, 34, 73, 11, 0, 6, 31, 16, 58, 2, 0),
+    ("aaron", "hicks", "nyy", "cf", 137, 480, 90, 119, 18, 3, 27, 79, 90, 111, 11, 2),
+    ("kyle", "higashioka", "nyy", "c",29, 72, 6, 12, 2, 0, 3, 6, 6, 16, 0, 0),
+    ("aaron", "judge", "nyy", "rf",112, 413, 77, 115, 22, 0, 27, 67, 76, 152, 6, 3),
+    ("andrew", "mccutchen", "nyy", "rf", 155, 569, 83, 145, 30, 3, 20, 65, 95, 145, 14, 9),
+    ("shane", "robinson", "nyy", "lf", 25, 49, 8, 7, 1, 0, 1, 2, 4, 6, 1, 0),
+    ("austin", "romine", "nyy", "c", 77, 242, 30, 59, 12, 0, 10, 42, 17, 67, 1, 0),
+    ("gary", "sanchez", "nyy", "c", 89, 323, 51, 60, 17, 0, 18, 53, 46, 94, 1, 0),
+    ("giancarlo", "stanton", "nyy", "dh", 158, 617, 102, 164, 34, 1, 38, 100, 70, 211, 5, 0),
+    ("gleyber", "torres", "nyy", "2b", 123, 431, 54, 117, 16, 1, 24, 77, 42, 122, 6, 2),
+    ("ronald", "torreyes", "nyy", "2b", 41, 100, 9, 28, 7, 1, 0, 7, 2, 16, 0, 0),
+    ("giovanny", "urshela", "nyy", "3b", 19, 43, 7, 10, 1, 0, 1, 3, 2, 10, 0, 0),
+    ("luke", "voit", "nyy", "1b", 47, 143, 30, 46, 5, 0, 15, 36, 17, 43, 0, 0),
+    ("tyler", "wade", "nyy", "ss", 36, 66, 8, 11, 4, 0, 1, 5, 4, 23, 1, 0),
+    ("neil", "walker", "nyy", "1b", 113, 347, 48, 76, 12, 1, 11, 46, 42, 87, 0, 0),
+]
---
+++ old_project/old_mainfile.py
@@ -0,0 +1,4 @@
+from player import Player
+from data import all_players
+from data import players_list
+
